e89229e1e9bc2449c495c0b597d11e42654ece00
Doris Lam <dlam@jpl.nasa.gov>
2025-01-13 14:25:03 -0800

    ci: fix gradle and ci

--- build.gradle.kts
+++ build.gradle.kts
@@ -70,3 +70,11 @@ tasks.jacocoTestReport {
         xml.required.set(true)
     }
 }
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
